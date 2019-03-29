b1888892df2f9f609a2c686498b3bb863d64297f
Jack Zhou <J.Zhou@televic.com>
2019-03-29 17:15:52 +0800

    add demo of 1_9

---
+++ chapter1/1_9.py
@@ -0,0 +1,10 @@
+import math
+import cmath
+# 在使用了 “from模块import函数” 这种形式的import命令之后，就可以直接使用函数，而不需要模块名作为前缀
+from math import sqrt
+
+print("math.floor(32.9) = ", math.floor(32.9))
+
+print("math.sqrt(9) = ", math.sqrt(9))
+
+print("cmath.sqrt(-1) = ", cmath.sqrt(-1))
