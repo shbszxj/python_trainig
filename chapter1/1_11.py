c467e1fd7fb55000e7a8011b958a57f2e74d38fe
Jack Zhou <J.Zhou@televic.com>
2019-04-29 16:36:01 +0800

    .

---
+++ chapter1/1_11.py
@@ -0,0 +1,12 @@
+# 使用反斜杠对字符串中的引号进行转义
+print("Let\'s go")
+
+x = "Hello "
+y = "world"
+print(x + y)
+
+# str函数，它会把值转换为合理形式的字符串，以便用户可以理解
+# repr会创建一个字符串，它以合法的Python表达式的形式来表示值
+print(repr("Hello world"))
+
+print(str("Hello world"))
