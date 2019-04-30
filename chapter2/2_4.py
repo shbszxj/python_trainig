a7d2dd694e035a300066c7322c8578519f804cbb
Jack Zhou <J.Zhou@televic.com>
2019-04-30 10:39:11 +0800

    add demo

---
+++ chapter2/2_4.py
@@ -0,0 +1,25 @@
+# 2.4 数字
+# 2.4.1 整数
+print(2+3)
+
+print(3-2)
+
+print(2*3)
+
+print(3/2)
+
+# 两个乘号标识乘方运算
+print(3**3)
+
+# 次序执行运算
+print(2 + 3 * 4)
+
+# 2.4.2 浮点数
+print(0.1 + 0.1)
+
+print(2 * 0.2)
+
+# 2.4.3 使用函数str()避免类型错误
+age = 23
+message = "Happy" + str(age) + "rd Birthday!"
+print(message)
